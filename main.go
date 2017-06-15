9e35784202074f2ff83f5529a11a7125b65f3738
Paul Ingles <paul@oobaloo.co.uk>
2017-06-15 10:44:31 +0100

    add flags for kubeconfig path

--- main.go
+++ main.go
@@ -9,7 +9,9 @@ import (
 	log "github.com/Sirupsen/logrus"
 	"github.com/uswitch/sqs-autoscaler-controller/pkg/scaler"
 	"github.com/uswitch/sqs-autoscaler-controller/pkg/tpr"
+	"gopkg.in/alecthomas/kingpin.v2"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
@@ -17,17 +19,34 @@ const (
 	kubecfg = "/home/tom/.kube/config"
 )
 
+type options struct {
+	kubeconfig string
+}
+
+func createClient(opts *options) (*kubernetes.Clientset, *rest.Config, error) {
+	config, err := clientcmd.BuildConfigFromFlags("", kubecfg)
+	if err != nil {
+		return nil, nil, err
+	}
+	c, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, nil, err
+	}
+	return c, config, nil
+}
+
 func main() {
-	// log.SetLevel(log.DebugLevel)
+	opts := &options{}
+	kingpin.Flag("kubeconfig", "Path to kubeconfig.").StringVar(&opts.kubeconfig)
+
 	stopChan := make(chan os.Signal)
 	signal.Notify(stopChan, os.Interrupt)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	config, err := clientcmd.BuildConfigFromFlags("", kubecfg)
-	c, err := kubernetes.NewForConfig(config)
+	c, config, err := createClient(opts)
 	if err != nil {
-		log.Fatalf("Error creating client: %s", err)
+		log.Fatalf("error creating client: %s", err)
 	}
 
 	err = tpr.EnsureResource(c)
